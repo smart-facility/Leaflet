5b7ad9dc2d795577efe2cae41430d2bae3a051ea
Ben Jones <ben.jones@selera.com>
2016-04-20 09:19:55 +1000

    Added message to copyright linking to fork on GitHub

--- dist/leaflet-src.js
+++ dist/leaflet-src.js
@@ -2,6 +2,8 @@
  Leaflet, a JavaScript library for mobile-friendly interactive maps. http://leafletjs.com
  (c) 2010-2013, Vladimir Agafonkin
  (c) 2010-2011, CloudMade
+
+ Forked by https://github.com/smart-facility/Leaflet
 */
 (function (window, document, undefined) {
 var oldL = window.L,
--- src/copyright.js
+++ src/copyright.js
@@ -2,4 +2,6 @@
  Leaflet, a JavaScript library for mobile-friendly interactive maps. http://leafletjs.com
  (c) 2010-2013, Vladimir Agafonkin
  (c) 2010-2011, CloudMade
+
+ Forked by https://github.com/smart-facility/Leaflet
 */
